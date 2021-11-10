15c88879f91933a6dc82fea85f1d664e23217c0a
AlissonBarreto <92763082+AlissonBarreto@users.noreply.github.com>
2021-11-10 12:53:31 -0300

    first commit

---
+++ Form2.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace RoboLoto
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.btnGerar = new System.Windows.Forms.Button();
+            this.rtbJogos = new System.Windows.Forms.RichTextBox();
+            this.numJogos = new System.Windows.Forms.NumericUpDown();
+            this.numNumero = new System.Windows.Forms.NumericUpDown();
+            this.numMin = new System.Windows.Forms.NumericUpDown();
+            this.numMax = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numJogos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numNumero)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMax)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGerar
+            // 
+            this.btnGerar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGerar.Location = new System.Drawing.Point(344, 12);
+            this.btnGerar.Name = "btnGerar";
+            this.btnGerar.Size = new System.Drawing.Size(63, 36);
+            this.btnGerar.TabIndex = 0;
+            this.btnGerar.Text = "Gerar";
+            this.btnGerar.UseVisualStyleBackColor = true;
+            this.btnGerar.Click += new System.EventHandler(this.btnGerar_Click);
+            // 
+            // rtbJogos
+            // 
+            this.rtbJogos.Location = new System.Drawing.Point(12, 54);
+            this.rtbJogos.Name = "rtbJogos";
+            this.rtbJogos.Size = new System.Drawing.Size(395, 200);
+            this.rtbJogos.TabIndex = 3;
+            this.rtbJogos.Text = "";
+            // 
+            // numJogos
+            // 
+            this.numJogos.Location = new System.Drawing.Point(12, 21);
+            this.numJogos.Name = "numJogos";
+            this.numJogos.Size = new System.Drawing.Size(73, 20);
+            this.numJogos.TabIndex = 4;
+            // 
+            // numNumero
+            // 
+            this.numNumero.Location = new System.Drawing.Point(91, 21);
+            this.numNumero.Name = "numNumero";
+            this.numNumero.Size = new System.Drawing.Size(73, 20);
+            this.numNumero.TabIndex = 5;
+            // 
+            // numMin
+            // 
+            this.numMin.Location = new System.Drawing.Point(168, 21);
+            this.numMin.Name = "numMin";
+            this.numMin.Size = new System.Drawing.Size(73, 20);
+            this.numMin.TabIndex = 6;
+            // 
+            // numMax
+            // 
+            this.numMax.Location = new System.Drawing.Point(247, 21);
+            this.numMax.Name = "numMax";
+            this.numMax.Size = new System.Drawing.Size(73, 20);
+            this.numMax.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Qtd. Jogos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(88, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Qtd. Numeros";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(165, 5);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Min";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(244, 5);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Max";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(420, 266);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numMax);
+            this.Controls.Add(this.numMin);
+            this.Controls.Add(this.numNumero);
+            this.Controls.Add(this.numJogos);
+            this.Controls.Add(this.rtbJogos);
+            this.Controls.Add(this.btnGerar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form2";
+            this.Text = "GoodLuck";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numJogos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numNumero)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMax)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGerar;
+        private System.Windows.Forms.RichTextBox rtbJogos;
+        private System.Windows.Forms.NumericUpDown numJogos;
+        private System.Windows.Forms.NumericUpDown numNumero;
+        private System.Windows.Forms.NumericUpDown numMin;
+        private System.Windows.Forms.NumericUpDown numMax;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RoboLoto
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGerar_Click(object sender, EventArgs e)
+        {
+            var rRandom = new Random();
+            int iQuantidade = decimal.ToInt32(numNumero.Value);
+            int iValorMinimo = decimal.ToInt32(numMin.Value);
+            int iValorMaximo = decimal.ToInt32(numMax.Value);
+            int iQuantidadeJogos = decimal.ToInt32(numJogos.Value);
+            int iDezena;
+            bool bExist;
+            int[] sNumerosGerados = new int[iQuantidade];
+            int iQuantidadeGerada = 0;
+
+            string sJogo = "";
+            rtbJogos.Text = "";
+
+            for (int x = 0; x < iQuantidadeJogos; x++)
+            {
+                sJogo = "";
+                iQuantidadeGerada = 0;
+
+                while (iQuantidadeGerada < iQuantidade)
+                {
+                    iDezena = rRandom.Next(iValorMinimo, iValorMaximo);
+                    bExist = false;
+
+                    for (int i = 0; i < sNumerosGerados.Count(); i++)
+                    {
+                        if (sNumerosGerados[i] == iDezena)
+                        {
+                            bExist = true;
+                            break;
+                        }
+                    }
+
+                    if (!bExist)
+                    {
+                        sNumerosGerados[iQuantidadeGerada] = iDezena;
+                        iQuantidadeGerada++;
+                    }
+                }
+
+                Array.Sort(sNumerosGerados);
+
+                for (int i = 0; i < sNumerosGerados.Count(); i++)
+                {
+                    sJogo += sNumerosGerados[i].ToString() + " ";
+                }
+
+                rtbJogos.Text += sJogo + Environment.NewLine;
+            }
+        }
+    }
+}
